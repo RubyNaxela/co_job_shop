d509117a58556a3b2ac8e410d9c0e1cb42f823f5
meti-odyn <117643767+meti-odyn@users.noreply.github.com>
2022-11-08 20:06:39 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <algorithm>
 #include "dataset.hpp"
 #include "schedule.hpp"
 
@@ -7,5 +8,16 @@
 int main() {
     js::dataset data = js::dataset::from_file("data.txt");
     js::schedule schedule;
+    //i<data.tasks.subtasks.size
+    for (int i=0; i<data.tasks[0].sequence.size();i++)
+    {
+        std::sort(data.tasks.begin(),data.tasks.end(),[=](const js::task& a, const js::task& b)
+        { return a.sequence[i].duration < b.sequence[i].duration; });
+
+        for(int j=0; j<data.tasks.size();j++)
+        {
+            schedule.add_sub_task(data.tasks[j].sequence[i]);
+        }
+    }
     return 0;
 }
