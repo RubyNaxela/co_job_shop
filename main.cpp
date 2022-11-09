592ca992e913cef3a66a63bad17f25ed0e2edb08
RubyNaxela <rubynaxela@gmail.com>
2022-11-09 03:21:15 +0100

    The scheduling algorithm doesn't change the dataset structure

--- main.cpp
+++ main.cpp
@@ -1,15 +1,19 @@
 #include <algorithm>
 #include <iostream>
+#include <numeric>
 #include "dataset.hpp"
 #include "schedule.hpp"
 
 // http://www.cs.put.poznan.pl/mdrozdowski/dyd/ok/index.html
 
 void stachu_algorithm(js::dataset& data, js::schedule& schedule) {
-    for (size_t i = 0; i < data.tasks[0].sequence.size(); i++) {
-        std::sort(data.tasks.begin(), data.tasks.end(),
-                  [=](const js::task& a, const js::task& b) { return a.sequence[i].duration > b.sequence[i].duration; });
-        for (auto& task : data.tasks) schedule.add_sub_task(task.sequence[i]);
+    const size_t sequence_length = data.tasks[0].sequence.size();
+    for (size_t i = 0; i < sequence_length; i++) {
+        std::vector<js::task*> tasks_order(data.tasks.size());
+        std::iota(tasks_order.begin(), tasks_order.end(), &data.tasks[0]);
+        std::sort(tasks_order.begin(), tasks_order.end(),
+                  [=](const js::task* a, const js::task* b) { return a->sequence[i].duration > b->sequence[i].duration; });
+        for (const auto& task : tasks_order) schedule.add_sub_task(task->sequence[i]);
     }
 }
 
