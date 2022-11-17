75a66a2b6e9975e7d2f9262c33f40cb0799d81d5
RubyNaxela <rubynaxela@gmail.com>
2022-11-17 04:38:11 +0100

    js::timeline class added

--- schedule.hpp
+++ schedule.hpp
@@ -10,13 +10,59 @@
 
 namespace js {
 
+    class timeline {
+
+        std::list<interval> intervals;
+
+    public:
+
+        typedef std::list<interval>::iterator pointer;
+
+        [[nodiscard]] pointer interval_at(time32_t time) {
+            return std::find_if(intervals.begin(), intervals.end(),
+                                [=](const interval i) { return i.includes(time); });
+        }
+
+        [[nodiscard]] time32_t length() const {
+            const interval& last = intervals.back();
+            if (last.occupied()) return last.end + 1;
+            else return last.start;
+        }
+
+        void add(interval&& interval) {
+            intervals.push_back(interval);
+        }
+
+        void insert_before(const pointer& it, interval&& interval) {
+            intervals.insert(it, interval);
+        }
+
+        void insert_after(const pointer& it, interval&& interval) {
+            intervals.insert(std::next(it), interval);
+        }
+
+        [[nodiscard]] std::vector<id32_t> quantized(time32_t limit) const {
+            std::vector<id32_t> result;
+            for (const auto& interval : intervals) {
+                for (time32_t i = interval.start; i <= interval.end and i < limit; i++) {
+                    if (interval.occupied()) result.push_back(interval.task->parent.id);
+                    else result.push_back(-1);
+                }
+            }
+            return result;
+        }
+
+        bool operator<(const timeline& other) const {
+            return length() < other.length();
+        }
+    };
+
     class schedule {
 
-        std::vector<std::list<interval>> table;
+        std::vector<timeline> table;
         dataset& data;
 
-        static void schedule_task(task& task, std::list<interval>& timeline,
-                                  std::list<interval>::iterator interval, time32_t start) {
+        static void schedule_task(task& task, timeline& timeline, const timeline::pointer& interval, time32_t start) {
 
             const time32_t interval_start = interval->start, interval_end = interval->end;
             const bool empty_before = start > interval_start, empty_after = start + task.duration - 1 < interval_end;
@@ -25,35 +71,22 @@ namespace js {
             interval->start = start;
             interval->end = start + task.duration - 1;
 
-            if (empty_before) timeline.insert(interval, interval::empty(interval_start, start - 1));
-            if (empty_after) timeline.insert(std::next(interval), interval::empty(start + task.duration, interval_end));
+            if (empty_before) timeline.insert_before(interval, interval::empty(interval_start, start - 1));
+            if (empty_after) timeline.insert_after(interval, interval::empty(start + task.duration, interval_end));
 
             task.parent.last_scheduled_time = start + task.duration;
             task.set_scheduled_time(&*interval);
         }
 
         [[nodiscard]] time32_t longest_timeline() const {
-            time32_t result = 0;
-            for (const auto& timeline : table)
-                if (timeline.back().occupied()) result = std::max(result, timeline.back().end);
-                else result = std::max(result, timeline.back().start);
-            return result;
-        }
-
-        static std::vector<id32_t> get_timeline(const std::list<interval>& timeline, time32_t length) {
-            std::vector<id32_t> result;
-            for (const auto& interval : timeline) {
-                for (time32_t i = interval.start; i <= interval.end and i < length; i++) {
-                    if (interval.occupied()) result.push_back(interval.task->parent.id);
-                    else result.push_back(-1);
-                }
-            }
-            return result;
+            return std::max_element(table.begin(), table.end())->length();
         }
 
 #ifndef WINDOZE
 
-        static std::string colored(const char* text, id32_t color) {
+        static std::string
+
+        colored(const char* text, id32_t color) {
             return "\033[1;" + std::to_string(31 + color % 6) + "m" + text + "\033[0m";
         }
 
@@ -65,15 +98,14 @@ namespace js {
 
     public:
 
-        explicit schedule(dataset& data) : table(std::vector<std::list<interval>>(data.machine_count)), data(data) {
-            for (auto& timeline : table) timeline.push_back(interval::empty());
+        explicit schedule(dataset& data) : table(std::vector<timeline>(data.machine_count)), data(data) {
+            for (auto& timeline : table) timeline.add(interval::empty());
         }
 
         void add_task(task& task) {
-            std::list<interval>& timeline = table[task.machine_id];
+            timeline& timeline = table[task.machine_id];
             const time32_t job_end = task.parent.last_scheduled_time;
-            auto time = std::find_if(timeline.begin(), timeline.end(),
-                                     [=](const interval i) { return i.includes(job_end); });
+            auto time = timeline.interval_at(job_end);
             while (time->occupied() or not time->includes(job_end, task.duration)) ++time;
             schedule_task(task, timeline, time, std::max(time->start, job_end));
         }
@@ -103,7 +135,7 @@ namespace js {
                 sprintf(id_string, fmt1, machine_id);
                 chart << id_string << ": ";
                 chart << '|';
-                for (id32_t task_id : get_timeline(table[machine_id], longest)) {
+                for (id32_t task_id : table[machine_id].quantized(longest)) {
                     sprintf(id_string, fmt3, task_id);
                     if (task_id == -1) chart << empty;
                     else chart << colored(id_string, task_id) << '|';
