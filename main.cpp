ba1f9012f126a5a4f7ee04057e019ff3e7cbf617
meti-odyn <117643767+meti-odyn@users.noreply.github.com>
2022-11-18 01:17:29 +0100

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,30 +1,56 @@
-#include <algorithm>
 #include <iostream>
-#include "dataset.hpp"
+#include <fstream>
+#include "dispatcher.hpp"
+#include "heuristics.hpp"
 #include "schedule.hpp"
 
 // http://www.cs.put.poznan.pl/mdrozdowski/dyd/ok/index.html
 
-void alex_algorithm(js::dataset& data, js::schedule& schedule) {
-    for (js::task& task : data.tasks) for (js::sub_task& sub_task : task.sequence) schedule.add_sub_task(sub_task);
-}
-
-void stachu_algorithm(js::dataset& data, js::schedule& schedule) {
-    for (int i = 0; i < data.tasks[0].sequence.size(); i++) {
-        std::sort(data.tasks.begin(), data.tasks.end(),
-                  [=](const js::task& a, const js::task& b) { return a.sequence[i].duration > b.sequence[i].duration; });
-        for (auto& task : data.tasks) schedule.add_sub_task(task.sequence[i]);
-    }
-}
-
 int main(int argc, char** argv) {
-    std::string data_file = "data_from_mary.txt";
-    if (argc > 1) data_file = std::string(argv[1]);
-    js::dataset data = js::dataset::from_file(data_file.c_str());
-    js::schedule schedule(data);
-    // alex_algorithm(data, schedule);
-    stachu_algorithm(data, schedule);
-    std::cout << schedule << std::endl;
-    std::cout << schedule.summary() << std::endl;
+
+    std::string data_file = "data.txt";
+    bool display_gantt_chart = false;
+    //std::string heuristic = "stachu_descending";
+    std::string path="results/new_results.txt";
+    std::string heuristics[] = {"alex_forward","alex_backward","stachu_ascending","stachu_descending"};
+    js::time32_t shortest_time = std::numeric_limits<js::time32_t>::max();
+    std::fstream file;
+
+    /*for (int i = 1; i < argc; i++) {
+        if (std::string(argv[i]) == "-d") data_file = argv[++i];
+        if (std::string(argv[i]) == "-g") display_gantt_chart = true;
+        if (std::string(argv[i]) == "-h") heuristic = argv[++i];
+    }*/
+
+    js::dataset data;
+    data.load_from_file(data_file.c_str());
+
+    for(int i=0;i<4;i++)
+    {
+        js::schedule schedule(data);
+        js::schedule_jobs(data, schedule, js::heuristics::get_by_name(heuristics[i]));
+        if(schedule.longest_timeline() < shortest_time)
+        {
+            file.open(path, std::ios::out);
+            file<<schedule.summary()<<std::endl;
+            file.close();
+            shortest_time = schedule.longest_timeline();
+        }
+        if(i==3)
+        {
+            if(shortest_time == std::numeric_limits<js::time32_t>::max())
+                std::cout << schedule.summary() << std::endl;
+            else
+            {
+                file.open(path, std::ios::in);
+                std::string line;
+                while(std::getline(file,line)) std::cout<<line;
+                file.close();
+            }
+        }
+    }
+
+    //if (display_gantt_chart) std::cout << schedule.gantt_chart() << std::endl;
+    
     return 0;
 }
--- schedule.hpp
+++ schedule.hpp
@@ -1,89 +1,157 @@
 #ifndef JOB_SHOP_SCHEDULE
 #define JOB_SHOP_SCHEDULE
 
+#include <list>
 #include <ostream>
 #include <sstream>
 #include <vector>
 #include "dataset.hpp"
+#include "platform.hpp"
 
 namespace js {
 
-    class schedule {
+    class timeline {
 
-        std::vector<std::vector<int16_t>> table;
-        dataset& data;
-
-        [[nodiscard]] int16_t get_cell(int16_t machine_id, size_t time_unit) const {
-            if (time_unit >= table[machine_id].size()) return -1;
-            return table[machine_id][time_unit];
-        }
-
-        bool is_available(const sub_task& task, size_t start) {
-            for (int16_t m = 0; m < (int16_t) table.size(); m++)
-                for (size_t t = start; t < start + task.duration; t++) {
-                    if (get_cell(task.machine_id, t) != -1 or get_cell(m, t) == task.task_id) return false;
-                }
-            return true;
-        }
-
-        void schedule_sub_task(std::vector<int16_t>& timeline, sub_task& task, size_t start) {
-            const size_t end = start + task.duration;
-            if (end >= timeline.size()) timeline.resize(end, -1);
-            for (size_t t = start; t < end; t++) timeline[t] = task.task_id;
-            task.scheduled_time = start;
-            data.get_task(task.task_id).last_scheduled_time = start + task.duration;
-        }
-
-        [[nodiscard]] size_t longest_timeline() const {
-            size_t result = 0;
-            for (const auto& timeline : table) result = std::max(result, timeline.size());
-            return result;
-        }
-
-        static std::string colored(const char* text, int16_t color) {
-            return "\033[1;" + std::to_string(31 + color % 6) + "m" + text + "\033[0m";
-        }
+        std::list<interval> intervals;
 
     public:
 
-        explicit schedule(dataset& data)
-                : table(std::vector<std::vector<int16_t>>(data.machine_count)), data(data) {}
+        typedef std::list<interval>::iterator pointer;
 
-        void add_sub_task(sub_task& task) {
-            std::vector<int16_t>& timeline = table[task.machine_id];
-            size_t t = data.get_task(task.task_id).last_scheduled_time;
-            while (not is_available(task, t)) t++;
-            schedule_sub_task(timeline, task, t);
+        [[nodiscard]] pointer interval_at(time32_t time) {
+            return std::find_if(intervals.begin(), intervals.end(),
+                                [=](const interval i) { return i.includes(time); });
         }
 
-        friend std::ostream& operator<<(std::ostream& os, const schedule& s) {
-            char* id_string = new char[3];
-            os << "     ";
-            for (uint8_t i = 0; i < 64; i++) {
-                sprintf(id_string, "%02hd", i);
-                os << id_string << " ";
-            }
-            os << std::endl;
-            for (int16_t machine_id = 0; machine_id < (int16_t) s.table.size(); machine_id++) {
-                sprintf(id_string, "%02hd", machine_id);
-                os << id_string << ": ";
-                os << '|';
-                for (int16_t task_id : s.table[machine_id]) {
-                    sprintf(id_string, "%02hd", task_id);
-                    if (task_id == -1) os << "__|";
-                    else os << colored(id_string, task_id) << '|';
+        [[nodiscard]] time32_t length() const {
+            const interval& last = intervals.back();
+            if (last.occupied()) return last.end + 1;
+            else return last.start;
+        }
+
+        void add(interval&& interval) {
+            intervals.push_back(interval);
+        }
+
+        void insert_before(const pointer& it, interval&& interval) {
+            intervals.insert(it, interval);
+        }
+
+        void insert_after(const pointer& it, interval&& interval) {
+            intervals.insert(std::next(it), interval);
+        }
+
+        [[nodiscard]] std::vector<id32_t> quantized(time32_t limit) const {
+            std::vector<id32_t> result;
+            for (const auto& interval : intervals) {
+                for (time32_t i = interval.start; i <= interval.end and i < limit; i++) {
+                    if (interval.occupied()) result.push_back(interval.task->parent.id);
+                    else result.push_back(-1);
                 }
-                os << std::endl;
             }
+            return result;
+        }
+
+        bool operator<(const timeline& other) const {
+            return length() < other.length();
+        }
+    };
+
+    class schedule {
+
+        std::vector<timeline> table;
+        dataset& data;
+
+        static void schedule_task(task& task, timeline& timeline, const timeline::pointer& interval, time32_t start) {
+
+            const time32_t interval_start = interval->start, interval_end = interval->end;
+            const bool empty_before = start > interval_start, empty_after = start + task.duration - 1 < interval_end;
+
+            interval->task = &task;
+            interval->start = start;
+            interval->end = start + task.duration - 1;
+
+            if (empty_before) timeline.insert_before(interval, interval::empty(interval_start, start - 1));
+            if (empty_after) timeline.insert_after(interval, interval::empty(start + task.duration, interval_end));
+
+            task.scheduled_time = start;
+            task.parent.last_scheduled_time = start + task.duration;
+        }
+
+#ifndef WINDOZE
+
+        static std::string
+
+        colored(const char* text, id32_t color) {
+            return "\033[1;" + std::to_string(31 + color % 6) + "m" + text + "\033[0m";
+        }
+
+#else
+        static std::string colored(const char* text, id32_t color) {
+            return text;
+        }
+#endif
+
+    public:
+
+        [[nodiscard]] time32_t longest_timeline() const {
+            return std::max_element(table.begin(), table.end())->length();
+        }
+
+        explicit schedule(dataset& data) : table(std::vector<timeline>(data.machine_count)), data(data) {
+            for (auto& timeline : table) timeline.add(interval::empty());
+        }
+
+        void add_task(task& task) {
+            timeline& timeline = table[task.machine_id];
+            const time32_t job_end = task.parent.last_scheduled_time;
+            auto time = timeline.interval_at(job_end);
+            while (time->occupied() or not time->includes(job_end, task.duration)) ++time;
+            schedule_task(task, timeline, time, std::max(time->start, job_end));
+        }
+
+        [[nodiscard]] std::string gantt_chart() {
+
+            std::stringstream chart;
+
+            const time32_t longest = longest_timeline();
+            const auto cell_width = uint8_t(std::max(log10(longest), log10(data.jobs.size())) + 1);
+            const auto left_col_width = uint8_t(log10(data.machine_count) + 1);
+            const std::string l_hd_format = "%0" + std::to_string(left_col_width) + "hd";
+            const std::string zu_format = "%0" + std::to_string(cell_width) + "zu";
+            const std::string hd_format = "%0" + std::to_string(cell_width) + "hd";
+            const char* fmt1 = l_hd_format.c_str(), * fmt2 = zu_format.c_str(), * fmt3 = hd_format.c_str();
+            const std::string empty = std::string(cell_width, '_') + '|';
+
+            chart << "   " + std::string(left_col_width, ' ');
+            char* id_string = new char[3];
+            for (time32_t i = 0; i < longest; i++) {
+                sprintf(id_string, fmt2, i);
+                chart << id_string << " ";
+            }
+            chart << std::endl;
+
+            for (id32_t machine_id = 0; machine_id < (id32_t) table.size(); machine_id++) {
+                sprintf(id_string, fmt1, machine_id);
+                chart << id_string << ": ";
+                chart << '|';
+                for (id32_t task_id : table[machine_id].quantized(longest)) {
+                    sprintf(id_string, fmt3, task_id);
+                    if (task_id == -1) chart << empty;
+                    else chart << colored(id_string, task_id) << '|';
+                }
+                chart << std::endl;
+            }
+
             delete[] id_string;
-            return os;
+            return chart.str();
         };
 
         [[nodiscard]] std::string summary() const {
             std::stringstream summary;
             summary << longest_timeline() << '\n';
-            std::vector<task>& tasks = data.tasks;
-            std::sort(tasks.begin(), tasks.end(), [](const task& a, const task& b) { return a.id < b.id; });
+            std::vector<job>& tasks = data.jobs;
+            std::sort(tasks.begin(), tasks.end(), [](const job& a, const job& b) { return a.id < b.id; });
             for (const auto& task : tasks) {
                 for (const auto& sub_task : task.sequence) summary << sub_task.scheduled_time << ' ';
                 summary << '\n';
